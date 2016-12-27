73e3c993ec66377bfc891d294f3da3014ec4f459
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2016-12-28 00:22:45 +0530

    Added entry point file.

---
+++ main.lisp
@@ -0,0 +1,5 @@
+;; Entry point for project. This page loads the various parts of the macro.
+;; You can choose not to load some of them if it's not useful.
+
+;; DON'T REMOVE THIS LINE
+(load "core.lisp")
